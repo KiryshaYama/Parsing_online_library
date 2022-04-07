fe427acc5e320fca6f167114967ef13f9852e0ba
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-07 21:37:19 +0500

    union of two parses
    union def parse_book_page and def parse_book_info

--- main.py
+++ main.py
@@ -31,39 +31,22 @@ def download_image(book_img_url, root_path=None, folder_name='images'):
     return file_path
 
 
-def parse_book_page(soup):
-    book_header_layout = soup.find('div', id='content').find('h1')
-    title, author = book_header_layout.text.split('::')
-
-    book_img_layout = soup.find('div', class_='bookimage').find('img')
-    book_img_url = book_img_layout['src']
-
-    comment_soups = soup.find_all('div', class_='texts')
-    comments = [comment_layout.find('span').text for comment_layout in comment_soups]
-
-    genres_soups = soup.find_all('span', class_='d_book')
-    genres = [genre.find('a').text for genre in genres_soups]
-
-    book_info = {
-        'title': title.strip(),
-        'author': author.strip(),
-        'book_img_url': book_img_url,
-        'comments':comments,
-        'genres':genres
-    }
-
-    return book_info
-
 def parse_book_info(book_id, url_template='https://tululu.org/b{book_id}/'):
     response = requests.get(
         url_template.format(book_id=book_id),allow_redirects=False)
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
-    book_info = parse_book_page(soup)
-
-    book_info['id'] = book_id
-    book_info['book_img_url'] = urljoin(response.url, book_info['book_img_url'])
+    book_header_layout = soup.find('div', id='content').find('h1')
+    title, author = book_header_layout.text.split('::')
+    book_img_layout = soup.find('div', class_='bookimage').find('img')
+    book_img_url = urljoin(response.url, book_img_layout['src'])
+    comment_soups = soup.find_all('div', class_='texts')
+    comments = [comment_layout.find('span').text for comment_layout in comment_soups]
+    genres_soups = soup.find_all('span', class_='d_book')
+    genres = [genre.find('a').text for genre in genres_soups]
+    book_info = {'title': title.strip(), 'author': author.strip(), 'book_img_url': book_img_url, 'comments': comments,
+                 'genres': genres, 'id': book_id, }
     return book_info
 
 
